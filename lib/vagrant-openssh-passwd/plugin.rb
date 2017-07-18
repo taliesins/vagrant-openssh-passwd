840e955e0138c0854921f1fe84c2bc6ef993c6ed
Taliesin Sisson <taliesins@interxion.com>
2017-07-18 16:18:07 +0100

    move action

--- lib/vagrant-openssh-passwd/plugin.rb
+++ lib/vagrant-openssh-passwd/plugin.rb
@@ -8,15 +8,13 @@ module VagrantPlugins
       This plugin ensures regenerates the passwd adnd group files for OpenSSH
       DESC
 
-      VAGRANT_VERSION_REQUIREMENT = ">= 1.1.0"
-
       action_hook(:generate_passwd, Plugin::ALL_ACTIONS) do |hook|
-        require_relative 'action/generate_passwd'
-        hook.after(Vagrant::Action::Builtin::SyncedFolders, Action::GeneratePasswd)
+        require_relative 'generate_passwd'
+        hook.after(Vagrant::Action::Builtin::SyncedFolders, GeneratePasswd)
       end
 
       config(:openssh_passwd) do
-        require_relative "config"
+        require_relative 'config'
         Config
       end
     end
