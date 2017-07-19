2d752af60db38396ba32977be2fd3b44d472bc35
Taliesin Sisson <taliesins@interxion.com>
2017-07-19 09:04:30 +0100

    Need to restart OpenSsh when configuration changes

--- lib/vagrant-openssh-passwd/generate_passwd.rb
+++ lib/vagrant-openssh-passwd/generate_passwd.rb
@@ -12,6 +12,7 @@ module VagrantPlugins
       end
 
       def needs_generate_passwd?
+        
         command = <<HERE
 $passwdPath = '#{@config.passwd_path}'
 if (!(Test-Path $passwdPath)) {throw "passwdPath does not exist - '$passwdPath'"}
@@ -70,7 +71,7 @@ if (!(Test-Path $groupPath)) {throw "groupPath does not exist - '$groupPath'"}
 $mkgroupPath = '#{@config.mkgroup_path}'
 if (!(Test-Path $mkgroupPath)) {throw "mkgroupPath does not exist - '$mkgroupPath'"}
 $group = &$groupPath -L
-$group | Set-Content $mkgroupPath -Encoding Ascii          
+$group | Set-Content $mkgroupPath -Encoding Ascii
 HERE
         @machine.communicate.shell.powershell(command) do |type, data|
           if type == :stderr
@@ -78,7 +79,19 @@ HERE
             raise Vagrant::Errors::VagrantError.new, "vagrant-openssh-passwd: unable to generate_group"
           end
         end
-      end          
+      end
+
+      def restart_openssh_service
+        command = <<HERE
+Restart-Service OpenSSHd -Force
+HERE
+        @machine.communicate.shell.powershell(command) do |type, data|
+          if type == :stderr
+            @machine.ui.error(data, prefix: false)
+            raise Vagrant::Errors::VagrantError.new, "vagrant-openssh-passwd: unable to restart_openssh_service"
+          end
+        end
+      end      
 
       # The middleware method that is invoked automatically by the Plugin ecosystem.
       # Expected to call the next middleware component in the chain if action should proceed.
@@ -94,15 +107,28 @@ HERE
             raise Vagrant::Errors::VagrantErrorr.new, "vagrant-openssh-passwd: communicator not ready!"
           end
 
-          if needs_generate_passwd?
-            @machine.ui.info("Running mkpasswd for OpenSSH")
-            generate_passwd
+          should_generate_passwd = false
+          if @config.generate_passwd 
+            should_generate_passwd = needs_generate_passwd?
+            if should_generate_passwd
+              @machine.ui.info("Running mkpasswd for OpenSSH")
+              generate_passwd
+            end
           end
 
-          if needs_generate_group?
-            @machine.ui.info("Running mkgroup for OpenSSH")
-            generate_group
-          end            
+          should_generate_group = false
+          if @config.generate_group 
+            should_generate_group = needs_generate_group?
+            if should_generate_group
+              @machine.ui.info("Running mkgroup for OpenSSH")
+              generate_group
+            end
+          end
+
+          if should_generate_passwd || should_generate_group
+            @machine.ui.info("Restarting OpenSSH service as there have been config changes")
+            restart_openssh_service
+          end
         else
           @logger.debug("No open ssh configuration to regenerate")
         end
